54b0f545a509e25eb5650c8fd82245f2d0e27a54
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-09-07 16:31:10 -0700

    Player names now clickable from overview page

--- src/components/playerOverview/PlayerOverview.tsx
+++ src/components/playerOverview/PlayerOverview.tsx
@@ -6,11 +6,13 @@ import { getPlayers } from "../../redux/statsSelectors";
 import { useSelector } from "react-redux";
 import { Loading } from "../Loading";
 import { Player } from "../../types/domain/Player";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Component showing all the players in a big list
  */
 export const PlayerOverview = React.memo(function MatchHistory() {
+	const navigate = useNavigate();
 	const players: { [id: string]: Player } | undefined = useSelector(getPlayers);
 
 	if (players === undefined) {
@@ -23,7 +25,19 @@ export const PlayerOverview = React.memo(function MatchHistory() {
 	return (
 		<Flex direction="column" justify="center" align="center">
 			<Heading>Player Overview</Heading>
-			<SortableTable columns={playerOverviewColumns} data={playersArray} />
+			<SortableTable
+				columns={playerOverviewColumns}
+				data={playersArray}
+				getRowProps={(row: any) => {
+					console.log(row);
+					return {
+						onClick: () => {
+							navigate(`/playerOverview/${row.original.name}`);
+							window.scrollTo(0, 0);
+						},
+					};
+				}}
+			/>
 		</Flex>
 	);
 });
