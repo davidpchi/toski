5d31f7b3184082c80424e9356bfd30b4aae3b23b
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-09-07 16:56:15 -0700

    Resolving requested changes
    Grouping imports, removing loading screen

--- src/components/playerOverview/PlayerDetails.tsx
+++ src/components/playerOverview/PlayerDetails.tsx
@@ -1,27 +1,22 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import { Loading } from "../Loading";
-import { getMatchesByPlayerName } from "../../redux/statsSelectors";
-import { useSelector } from "react-redux";
-import { AppState } from "../../redux/rootReducer";
 import { Flex, Heading, Text } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+
+import { getMatchesByPlayerName } from "../../redux/statsSelectors";
+import { AppState } from "../../redux/rootReducer";
 
 export async function loader(data: { params: any }) {
 	return data.params.playerId;
 }
 
 export const PlayerDetails = React.memo(function PlayerDetails() {
-	// Key player variables
+	// Player variables
 	const playerId = useLoaderData() as string;
 	const title = playerId;
 	const matches = useSelector((state: AppState) =>
 		getMatchesByPlayerName(state, playerId ? playerId : ""),
 	);
-
-	if (playerId === undefined) {
-		return <Loading text="" />;
-	}
-	console.log(matches);
 
 	// Calculate metrics (number of games, win rate)
 	const numberOfMatches = matches.length;
