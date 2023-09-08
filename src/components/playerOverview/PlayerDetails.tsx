75f523b45a1fe8047d91b17db27ad5a901853d16
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-09-07 21:43:45 -0700

    Added match history to player details

--- src/components/playerOverview/PlayerDetails.tsx
+++ src/components/playerOverview/PlayerDetails.tsx
@@ -1,22 +1,31 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { Flex, Heading, Text } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 
 import { getMatchesByPlayerName } from "../../redux/statsSelectors";
 import { AppState } from "../../redux/rootReducer";
+import { matchHistoryColumns } from "../matchHistory/matchHistoryColumnHelper";
+import { SortableTable } from "../SortableTable";
+import { Loading } from "../Loading";
 
 export async function loader(data: { params: any }) {
 	return data.params.playerId;
 }
 
 export const PlayerDetails = React.memo(function PlayerDetails() {
+	const navigate = useNavigate();
+
 	// Player variables
 	const playerId = useLoaderData() as string;
 	const title = playerId;
 	const matches = useSelector((state: AppState) =>
 		getMatchesByPlayerName(state, playerId ? playerId : ""),
 	);
+
+	if (matches.length === 0) {
+		return <Loading text="Loading..." />;
+	}
 
 	// Calculate metrics (number of games, win rate)
 	const numberOfMatches = matches.length;
@@ -26,6 +35,7 @@ export const PlayerDetails = React.memo(function PlayerDetails() {
 			numberOfWins++;
 		}
 	}
+
 	const playerWinRate =
 		numberOfMatches > 0
 			? Math.round((numberOfWins * 100) / numberOfMatches)
@@ -33,11 +43,27 @@ export const PlayerDetails = React.memo(function PlayerDetails() {
 
 	return (
 		<Flex direction="column" justify="center" align="center">
-			<Heading>{title}</Heading>
-			<Flex direction="column" padding="16px">
-				<Text>{`Total Number of Games: ${numberOfMatches}`}</Text>
-				<Text>{`Winrate: ${playerWinRate}%`}</Text>
+			<Flex>
+				<Heading>{title}</Heading>
+
+				<Flex direction="column" padding="16px">
+					<Text> {`Total Number of Games: ${numberOfMatches}`}</Text>
+					<Text>{`Winrate: ${playerWinRate}%`}</Text>
+				</Flex>
 			</Flex>
+
+			<SortableTable
+				columns={matchHistoryColumns}
+				data={matches}
+				getRowProps={(row: any) => {
+					return {
+						onClick: () => {
+							navigate(`/matchHistory/${row.original.id}`);
+							window.scrollTo(0, 0);
+						},
+					};
+				}}
+			/>
 		</Flex>
 	);
 });
