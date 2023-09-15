ffe022659482bc8fecb11220dfdd36ae6288693e
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-09-15 12:52:10 -0700

    Resolve missing close parens issue

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -4,9 +4,9 @@ export const COMMANDER_MINIMUM_GAMES_REQUIRED = 5;
 export const MTG_COLOR_NAMES = ["Black", "Green", "Red", "Blue", "White"];
 
 export const MTG_COLOR_RGB = [
-    "rgb(166, 159, 157",
-    "rgb(196, 211, 202",
-    "rgb(235, 159, 130",
-    "rgb(179, 206, 234",
-    "rgb(248, 231, 185",
+    "rgb(166, 159, 157)",
+    "rgb(196, 211, 202)",
+    "rgb(235, 159, 130)",
+    "rgb(179, 206, 234)",
+    "rgb(248, 231, 185)",
 ];
