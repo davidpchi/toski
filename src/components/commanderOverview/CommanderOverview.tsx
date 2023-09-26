c933209a69cd784b674178990b02066541b7c45f
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-09-26 10:21:41 -0700

    Added piegraph to commander overview Final formatting of header completely up to site overlords

--- src/components/commanderOverview/CommanderOverview.tsx
+++ src/components/commanderOverview/CommanderOverview.tsx
@@ -7,17 +7,21 @@ import { useSelector } from "react-redux";
 import { Loading } from "../Loading";
 import { Commander } from "../../types/domain/Commander";
 import { useNavigate } from "react-router-dom";
-import { COMMANDER_MINIMUM_GAMES_REQUIRED } from "../constants";
+import { COMMANDER_MINIMUM_GAMES_REQUIRED, MTG_COLORS } from "../constants";
 import { AppState } from "../../redux/rootReducer";
 import { DatePicker } from "../common/DatePicker";
+import { PieGraph } from "../dataVisualizations/PieGraph";
 
 export const CommanderOverview = React.memo(function MatchHistory() {
     const navigate = useNavigate();
 
     const [dateFilter, setDateFilter] = useState<Date | undefined>(undefined);
-    const onDatePickerChange = useCallback((date: Date | undefined) => {
-        setDateFilter(date);
-    }, [setDateFilter])
+    const onDatePickerChange = useCallback(
+        (date: Date | undefined) => {
+            setDateFilter(date);
+        },
+        [setDateFilter],
+    );
 
     const allCommanders = useSelector(getCommanders);
     const commanders: Commander[] = useSelector((state: AppState) => getCommandersByDate(state, dateFilter));
@@ -33,31 +37,56 @@ export const CommanderOverview = React.memo(function MatchHistory() {
 
     let commandersArray = commanders.sort((a: Commander, b: Commander) => a.name.localeCompare(b.name));
     if (isFiltered && allCommanders) {
-        commandersArray = commandersArray.filter(
-            (value: Commander) => allCommanders[value.id].matches.length >= COMMANDER_MINIMUM_GAMES_REQUIRED,
-        );
+        commandersArray = commandersArray.filter((value: Commander) => allCommanders[value.id].matches.length >= COMMANDER_MINIMUM_GAMES_REQUIRED);
     }
 
+    // Create a color dictionary to track colors played
+    const colorsPlayedDictionary: Record<string, number> = MTG_COLORS.reduce<Record<string, number>>((acc, color) => {
+        acc[color.id] = 0;
+        return acc;
+    }, {});
+    // Loop through all commanders and update dictionary
+    for (const commander of commandersArray) {
+        for (const colorID of commander.colorIdentity) {
+            colorsPlayedDictionary[colorID] += commander.matches.length;
+        }
+    }
+    // Convert dictionary to array for display in pie
+    const colorsPlayedArray: number[] = [];
+    for (const colorObj of MTG_COLORS) {
+        colorsPlayedArray.push(colorsPlayedDictionary[colorObj.id]);
+    }
 
     return (
         <Flex direction="column" justify="center" align="center">
             <Heading>Commander Overview</Heading>
-            <Flex alignSelf={"center"} marginBottom={"16px"} alignItems={"center"}>
-                <DatePicker onChange={onDatePickerChange} />
-                <Tooltip
-                    label={<p style={{ textAlign: "center" }}>Commanders play 5 games to be qualified.</p>}
-                    hasArrow
-                    arrowSize={15}
-                >
-                    <div>
-                        <Checkbox isChecked={isFiltered} onChange={onFilterChange}>
-                            {"Show only qualified"}
-                        </Checkbox>
-                    </div>
-                </Tooltip>
+            <Flex direction="row" gap="30px" marginBottom="20px">
+                <Flex>
+                    <Flex maxWidth={175} maxHeight={175}>
+                        <div style={{ flex: 1, display: "flex", width: "100%", height: "100%" }}>
+                            <PieGraph
+                                dataLabel={"Commanders played"}
+                                data={colorsPlayedArray}
+                                backgroundColors={MTG_COLORS.map((color) => color.rgb)}
+                            />
+                        </div>
+                    </Flex>
+                </Flex>
+
+                <Flex direction="column" justifyContent="center" alignItems="flex-start">
+                    <DatePicker onChange={onDatePickerChange} />
+                    <Tooltip label={<p style={{ textAlign: "center" }}>Commanders play 5 games to be qualified.</p>} hasArrow arrowSize={15}>
+                        <div>
+                            <Checkbox isChecked={isFiltered} onChange={onFilterChange}>
+                                {"Show only qualified"}
+                            </Checkbox>
+                        </div>
+                    </Tooltip>
+                </Flex>
             </Flex>
-            {
-                commandersArray.length ? <SortableTable
+
+            {commandersArray.length ? (
+                <SortableTable
                     columns={commanderOverviewColumns}
                     data={commandersArray}
                     getRowProps={(row: any) => {
@@ -68,8 +97,10 @@ export const CommanderOverview = React.memo(function MatchHistory() {
                             },
                         };
                     }}
-                /> : <div style={{ textAlign: "center" }}>No data</div>
-            }
+                />
+            ) : (
+                <div style={{ textAlign: "center" }}>No data</div>
+            )}
         </Flex>
     );
 });
