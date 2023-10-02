dfb7c1ef7894c7e937f0aa90e219b58ab9899c26
David <davidpchi@gmail.com>
2023-10-02 08:45:53 -0700

    More work on match submit

--- src/components/matchHistory/MatchSubmission.tsx
+++ src/components/matchHistory/MatchSubmission.tsx
@@ -2,14 +2,17 @@ import React, { useCallback, useMemo, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-import { Button, Flex, Heading, Input, Select, Text, Image, IconButton } from "@chakra-ui/react";
+import { Button, Flex, Heading, Input, Select, Text, Image, IconButton, Box, Skeleton, Slider, SliderTrack, SliderFilledTrack, SliderThumb, SliderMark, Textarea } from "@chakra-ui/react";
 import { submitMatch } from "../../services/matchHistoryService";
 import { CreatableSelect, SingleValue } from "chakra-react-select";
 import { useSelector } from "react-redux";
 import { getPlayers } from "../../redux/statsSelectors";
 import { commanderList } from "../../services/commanderList";
-import { FiX } from "react-icons/fi";
+import { FiUserPlus, FiX } from "react-icons/fi";
 import { useNavigate } from "react-router";
+import { primaryColor } from "../../themes/acorn";
+
+const placeholderImage = "https://static.thenounproject.com/png/5425-200.png";
 
 type SelectItem = {
     name: string,
@@ -19,15 +22,13 @@ type SelectItem = {
 const MatchSubmissionPlayerCard = React.memo(function MatchSubmissionPlayerCard({
     title,
     commanderOptions,
-    rankValue,
     showCloseIcon = false,
     setPlayerValue,
     setCommanderValue,
     setPlayerRank,
-    onClose
+    onClose,
 }: {
     title: string,
-    rankValue: number,
     commanderOptions: SelectItem[],
     showCloseIcon?: boolean,
     setPlayerValue: (val: string) => void,
@@ -64,12 +65,15 @@ const MatchSubmissionPlayerCard = React.memo(function MatchSubmissionPlayerCard(
         setCommanderValue(value);
     };
 
-    const onUpdateRank = useCallback((event: any) => {
-        setPlayerRank(Number(event.target.value));
-    }, []);
 
-    const commanderImage = commanderList[commanderSelectValue] ? commanderList[commanderSelectValue].image.replace("normal", "art_crop") : "";
+    const [rankSliderValue, setRankSliderValue] = useState<number>(1);
 
+    const onUpdateRank = useCallback((value: number) => {
+        setRankSliderValue(value);
+        setPlayerRank(value);
+    }, [setPlayerRank]);
+
+    const commanderImage = commanderList[commanderSelectValue] ? commanderList[commanderSelectValue].image.replace("normal", "art_crop") : undefined;
 
     return (
         <Flex
@@ -82,21 +86,32 @@ const MatchSubmissionPlayerCard = React.memo(function MatchSubmissionPlayerCard(
             marginBottom={"16px"}
             marginRight={"16px"}
             marginTop={"16px"}
-            width={"500px"}>
-            <Flex justifyContent={"space-between"} >
-                <Flex direction={"column"}>
-                    <Heading size={"md"} padding={"0px"}>{title}</Heading>
-                    <Image src={commanderImage} height={20} borderRadius={8} />
+            width={"100%"}
+            maxWidth={"500px"}>
+            <div style={{ position: "relative" }}>
+                <Flex
+                    direction={"column"}
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    flex={1}>
+                    <Heading size={"md"} padding={"0px"} marginBottom={"8px"}>{title}</Heading>
+                    {commanderImage !== undefined ?
+                        <Image src={commanderImage} height={20} borderRadius={8} />
+                        : <Image src={placeholderImage} height={"80px"} borderRadius={8} />
+                    }
                 </Flex>
                 {showCloseIcon ?
                     <IconButton
+                        position={"absolute"}
+                        top={0}
+                        right={0}
                         onClick={onClose}
                         variant='ghost'
                         aria-label='open menu'
                         icon={<FiX />}
                     /> : null}
-            </Flex>
-            <Text>Name:</Text>
+            </div>
+            <Text marginTop={"8px"}>Name:</Text>
             <CreatableSelect
                 isMulti={false}
                 options={playersArray}
@@ -111,7 +126,7 @@ const MatchSubmissionPlayerCard = React.memo(function MatchSubmissionPlayerCard(
                 size='lg'
             />
 
-            <Text>Commander:</Text>
+            <Text marginTop={"8px"}>Commander:</Text>
             <Select
                 size='lg'
                 onChange={onCommanderSelectChange}
@@ -122,8 +137,20 @@ const MatchSubmissionPlayerCard = React.memo(function MatchSubmissionPlayerCard(
                     return (<option value={option.name}>{option.name}</option>)
                 })}
             </Select>
-            <Text>Rank:</Text>
-            <Input value={rankValue} onChange={onUpdateRank} marginBottom={"16px"}></Input>
+            <Text marginTop={"8px"}>Rank:</Text>
+            <Box padding={"6px"}>
+                <Slider defaultValue={1} min={1} max={4} step={1} onChangeEnd={onUpdateRank}>
+                    <SliderMark value={1} marginTop={"10px"}>1</SliderMark>
+                    <SliderMark value={2} marginTop={"10px"}>2</SliderMark>
+                    <SliderMark value={3} marginTop={"10px"}>3</SliderMark>
+                    <SliderMark value={4} marginTop={"10px"}>4</SliderMark>
+                    <SliderTrack bg={primaryColor[100]} >
+                        <Box position='relative' right={10} />
+                        <SliderFilledTrack bg={primaryColor[500]} />
+                    </SliderTrack>
+                    <SliderThumb boxSize={6} />
+                </Slider>
+            </Box>
         </Flex >
     )
 })
@@ -135,19 +162,19 @@ export const MatchSubmission = React.memo(function MatchSubmission() {
 
     const [player1Name, setPlayer1Name] = useState<string>("");
     const [player1Commander, setPlayer1Commander] = useState<string>("");
-    const [player1Rank, setPlayer1Rank] = useState<number>(0);
+    const [player1Rank, setPlayer1Rank] = useState<number>(1);
 
     const [player2Name, setPlayer2Name] = useState<string>("");
     const [player2Commander, setPlayer2Commander] = useState<string>("");
-    const [player2Rank, setPlayer2Rank] = useState<number>(0);
+    const [player2Rank, setPlayer2Rank] = useState<number>(1);
 
     const [player3Name, setPlayer3Name] = useState<string>("");
     const [player3Commander, setPlayer3Commander] = useState<string>("");
-    const [player3Rank, setPlayer3Rank] = useState<number>(0);
+    const [player3Rank, setPlayer3Rank] = useState<number>(1);
 
     const [player4Name, setPlayer4Name] = useState<string>("");
     const [player4Commander, setPlayer4Commander] = useState<string>("");
-    const [player4Rank, setPlayer4Rank] = useState<number>(0);
+    const [player4Rank, setPlayer4Rank] = useState<number>(1);
 
     const [turnCount, setTurnCount] = useState<number>(0);
 
@@ -165,6 +192,12 @@ export const MatchSubmission = React.memo(function MatchSubmission() {
 
     const onClose = useCallback(() => {
         setPlayerCount(playerCount - 1);
+    }, [playerCount, setPlayerCount])
+
+    const onAddPlayer = useCallback(() => {
+        if (playerCount < 4) {
+            setPlayerCount(playerCount + 1);
+        }
     }, [playerCount, setPlayerCount])
 
     const submitData = useCallback(async () => {
@@ -234,55 +267,59 @@ export const MatchSubmission = React.memo(function MatchSubmission() {
             <Flex direction='column' justify='center' align='left' marginBottom={"16px"}>
                 <Text>Date:</Text>
                 <DatePicker selected={date} onChange={(value) => { if (value !== null) setDate(value); }} />
-                <Flex direction='column' justify='center' align='left' marginBottom={"16px"}>
-                    {playerCount >= 1 ?
-                        <MatchSubmissionPlayerCard
-                            commanderOptions={commandersArray}
-                            title={"Player 1"}
-                            rankValue={player1Rank}
-                            setPlayerValue={setPlayer1Name}
-                            setCommanderValue={setPlayer1Commander}
-                            setPlayerRank={setPlayer1Rank} />
-                        : null}
-                    {playerCount >= 2 ?
-                        <MatchSubmissionPlayerCard
-                            commanderOptions={commandersArray}
-                            title={"Player 2"}
-                            rankValue={player2Rank}
-                            setPlayerValue={setPlayer2Name}
-                            setCommanderValue={setPlayer2Commander}
-                            setPlayerRank={setPlayer2Rank}
-                            showCloseIcon={playerCount == 2}
-                            onClose={onClose} />
-                        : null}
-                    {playerCount >= 3 ?
-                        <MatchSubmissionPlayerCard
-                            commanderOptions={commandersArray}
-                            title={"Player 3"}
-                            rankValue={player3Rank}
-                            setPlayerValue={setPlayer3Name}
-                            setCommanderValue={setPlayer3Commander}
-                            setPlayerRank={setPlayer3Rank}
-                            showCloseIcon={playerCount == 3}
-                            onClose={onClose} />
-                        : null}
-                    {playerCount >= 4 ?
-                        <MatchSubmissionPlayerCard
-                            commanderOptions={commandersArray}
-                            title={"Player 4"}
-                            rankValue={player4Rank}
-                            setPlayerValue={setPlayer4Name}
-                            setCommanderValue={setPlayer4Commander}
-                            setPlayerRank={setPlayer4Rank}
-                            showCloseIcon={playerCount == 4}
-                            onClose={onClose} />
-                        : null}
-                </Flex>
+            </Flex>
+            <Flex direction='column' justify='center' align='left' marginBottom={"16px"}>
+                {playerCount >= 1 ?
+                    <MatchSubmissionPlayerCard
+                        commanderOptions={commandersArray}
+                        title={"Player 1"}
+                        setPlayerValue={setPlayer1Name}
+                        setCommanderValue={setPlayer1Commander}
+                        setPlayerRank={setPlayer1Rank} />
+                    : null}
+                {playerCount >= 2 ?
+                    <MatchSubmissionPlayerCard
+                        commanderOptions={commandersArray}
+                        title={"Player 2"}
+                        setPlayerValue={setPlayer2Name}
+                        setCommanderValue={setPlayer2Commander}
+                        setPlayerRank={setPlayer2Rank}
+                        showCloseIcon={playerCount == 2}
+                        onClose={onClose} />
+                    : null}
+                {playerCount >= 3 ?
+                    <MatchSubmissionPlayerCard
+                        commanderOptions={commandersArray}
+                        title={"Player 3"}
+                        setPlayerValue={setPlayer3Name}
+                        setCommanderValue={setPlayer3Commander}
+                        setPlayerRank={setPlayer3Rank}
+                        showCloseIcon={playerCount == 3}
+                        onClose={onClose} />
+                    : null}
+                {playerCount >= 4 ?
+                    <MatchSubmissionPlayerCard
+                        commanderOptions={commandersArray}
+                        title={"Player 4"}
+                        setPlayerValue={setPlayer4Name}
+                        setCommanderValue={setPlayer4Commander}
+                        setPlayerRank={setPlayer4Rank}
+                        showCloseIcon={playerCount == 4}
+                        onClose={onClose} />
+                    : null}
+                {playerCount < 4 ?
+                    <Button onClick={onAddPlayer} width={"100%"}>
+                        <Flex direction={"row"} alignItems={"center"}>
+                            <FiUserPlus />
+                            <Text size={"md"} padding={"0px"} marginLeft={"16px"} >Add Player</Text>
+                        </Flex>
+                    </Button>
+                    : null}
             </Flex>
             <Text>Number of Turns:</Text>
-            <Input value={turnCount} onChange={onUpdateTurnCount} marginBottom={"16px"} width={"500px"}></Input>
+            <Input value={turnCount} onChange={onUpdateTurnCount} marginBottom={"16px"} width={"100%"} maxWidth={"500px"}></Input>
             <Text>Notes:</Text>
-            <Input value={notes} onChange={onUpdateNotes} marginBottom={"16px"} width={"500px"}></Input>
+            <Textarea value={notes} onChange={onUpdateNotes} marginBottom={"16px"} width={"100%"} maxWidth={"500px"}></Textarea>
             <Button onClick={submitData}>Submit Data</Button>
         </Flex>
     )
