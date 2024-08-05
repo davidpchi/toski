817e854391691d3b88d23be53e711c71d0826d3d
David <davidpchi@gmail.com>
2024-08-04 22:15:54 -0700

    Allow  user to change toksi to discord mapping

--- src/components/admin/AdminScreen.tsx
+++ src/components/admin/AdminScreen.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ProfileSelectors } from "../../redux/profiles/profilesSelectors";
 import { useSelector } from "react-redux";
-import { Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { Table, Tbody, Td, Th, Thead, Tr, Text, Flex, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { useUserInfo } from "../../logic/hooks/userHooks";
+import { ProfileService } from "../../services/ProfileService";
 
 export const Admin = React.memo(function Admin() {
     const navigate = useNavigate();
+    const updateProfileLink = ProfileService.useUpdateProfileLink();
     const { userId } = useUserInfo();
     const profiles = useSelector(ProfileSelectors.getProfiles);
 
@@ -23,16 +25,31 @@ export const Admin = React.memo(function Admin() {
 
     if (currentUser === undefined || !currentUser.isAdmin) {
         navigate("/", { replace: true });
-        return <div></div>;
+        return <div />;
     }
 
     const tableData = [];
-    for (const value of Object.values(profiles)) {
+    for (const profile of Object.values(profiles)) {
+        // TODO: this should be moved into a component
+        const updateToskiId = () => {
+            const entry = prompt("Enter toski Id");
+            if (entry) {
+                updateProfileLink(profile.id, entry);
+            }
+        };
+
         tableData.push(
             <Tr>
-                <Td>{value.id}</Td>
-                <Td>{value.toskiId}</Td>
-                <Td>{value.moxfieldId}</Td>
+                <Td>{profile.id}</Td>
+                <Td>
+                    <Flex direction={"row"} justifyContent={"space-between"}>
+                        <Text>{profile.toskiId}</Text>
+                        <Button onClick={updateToskiId} size={"sm"} variant={"ghost"}>
+                            <Text>Update Toski Id</Text>
+                        </Button>
+                    </Flex>
+                </Td>
+                <Td>{profile.moxfieldId}</Td>
             </Tr>
         );
     }
